a9066df2196ac5e4c6197e387cfe16c4950dd100
Guilherme Casagrande <thegui4000@gmail.com>
2022-08-23 15:07:37 -0300

    created repository

---
+++ conect/conexao.php
@@ -0,0 +1,9 @@
+<?php
+$host="localhost";
+$usuario="root";
+$senha="";
+$banco="vendas";
+
+$conexao=mysqli_connect($host,$usuario,$senha,$banco);
+
+?>
---
+++ controller/salvar.php
@@ -0,0 +1,18 @@
+<?php
+$cliente = $_POST['cliente'];
+$cpf = $_POST['cpf'];
+$cep = $_POST['cep'];
+$endereco = $_POST['endereco'];
+$cidade = $_POST['cidade'];
+$estado = $_POST['estado'];
+$telefone = $_POST['fone'];
+$email = $_POST['email'];
+
+include("../conect/conexao.php");
+$sql = "INSERT INTO `clientes`(`nome_clie`, `cpf_clie`, `cep_clie`, `end_clie`, `cidade_clie`, `uf_clie`, `fone_clie`, `email_clie`) VALUES ('$cliente','$cpf','$cep','$endereco','$cidade','$estado','$telefone','$email')";
+
+mysqli_query($conexao, $sql);
+
+header("location: ../view/index.html");
+
+?>
